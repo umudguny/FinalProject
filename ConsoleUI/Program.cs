db696586912080d1c8d70e9e25d03c23691c9b92
umudguny <ug152486@gmail.com>
2023-11-19 23:05:36 +0300

    s

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductTest();
+//CategoryTest();
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}
+
+static void ProductTest()
+{
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    var result = productManager.GetProductDetails();
+
+    if (result.Success==true)
+    {
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+        }
+    }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server,Postgres,MongoDb
+            _products = new List<Product> { 
+                new Product{ProductId=1,CategoryId=1, ProductName="Bardak",UnitPrice=38,UnitsInStock=16},
+                new Product{ProductId=2,CategoryId=1, ProductName="Bilgisayar",UnitPrice=27000,UnitsInStock=13},
+                new Product{ProductId=3,CategoryId=2, ProductName="Telefon",UnitPrice=13000,UnitsInStock=9},
+                new Product{ProductId=4,CategoryId=2, ProductName="Masa",UnitPrice=2610,UnitsInStock=34},
+                new Product{ProductId=5,CategoryId=2, ProductName="Kitap",UnitPrice=390,UnitsInStock=26}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ- Language Integrated Query
+            Product productToDelete= _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            //gönderdiğim ürün idsine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p=> p.ProductId==product.ProductId);
+            productToUpdate.ProductName=product.ProductName;
+            productToUpdate.CategoryId=product.CategoryId;
+            productToUpdate.UnitPrice=product.UnitPrice;
+            productToUpdate.UnitsInStock=product.UnitsInStock;
+        }
+    }
+}
